e31d05fb83c72dda10eb64c22cd09ebc52d5387e
Murat Alperen Ulutas <murat.alperenulutas@hotmail.com>
2025-11-24 16:15:17 +0300

    feat: add dotenv configurations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configService.get('PORT', 3000));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
