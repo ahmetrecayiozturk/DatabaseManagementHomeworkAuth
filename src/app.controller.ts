539d9191eb6fcec5df282b3d7c6176e653f5663b
Murat Alperen Ulutas <murat.alperenulutas@hotmail.com>
2025-11-24 15:52:40 +0300

    feat: Update AppController and AppService to return server status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getServerRunning(): string {
+    return this.appService.getServerRunning();
   }
 }
